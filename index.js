45330297025ca96ad8dff6a29fc9ae216567a893
razeeth11 <razeeth011@gmail.com>
2023-03-16 11:41:03 +0530

    Connected with mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import express from 'express'
-
-
+import express from "express";
+import { MongoClient } from "mongodb";
+import cors from 'cors'
 
 const PORT = 3232;
+
+const URL = "mongodb://127.0.0.1";
+const client = new MongoClient(URL);
+client.connect();
+
 const app = express();
 
-app.get("/" , function(request,response){
-  response.send("Hello")
-})
+app.use(cors())
 
-app.listen(PORT, ()=> console.log(`Server started in PORT ${PORT}`))
+app.get("/", function (request, response) {
+  response.send("Hello");
+});
+
+app.post("/mobData", express.json() ,async function (request, response) {
+  const data = request.body
+  const mobData = await client.db('React_tiny').collection('React_tiny_data').insertMany(data)
+  response.send(mobData);
+});
+
+app.get("/data",async function (request, response) {
+  const data =await client.db("React_tiny").collection("React_tiny_data").find({}).toArray();
+
+  response.send(data)
+});
+
+app.listen(PORT, () => console.log(`Server started in PORT ${PORT}`));
